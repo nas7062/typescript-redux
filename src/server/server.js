ad6792f9d76d5f35e4261aaaee2d2567f56a89a8
10012 <nas7062@naver.com>
2024-08-08 11:43:50 +0900

    add

--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,7 @@
     const server = http.createServer(app);
     const io = new Server(server,{
         cors :{
-            origin: "*"
+            origin:  "https://10012-trd.vercel.app"
         },
         transports: ['websocket','polling']
     });
