91cf632524f3558840bb9c1d9c79d5847e5cd3e2
10012 <nas7062@naver.com>
2024-08-19 11:38:15 +0900

    ADD

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -6,7 +6,7 @@ let socket: Socket | null = null;
 
 export const ConnectToServer = (username: string) => {
     if (!socket) {
-        socket = io("https://your-ec2-13.59.107.97", {
+        socket = io("https://your-ec2-13.59.107.97:3000", {
             autoConnect: false,
             path: "/socket.io/",
             query: { username },
